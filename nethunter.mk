dad295c75b90880818751238d3d48a79d85fe5b0
binkybear <binkybear@nethunter.com>
2016-11-17 18:09:05 -0600

    Nethunterfy
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- nethunter.mk
+++ nethunter.mk
@@ -20,9 +20,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/oneplus2/device.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/nethunter/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_oneplus2
+PRODUCT_NAME := nethunter_oneplus2
 PRODUCT_DEVICE := oneplus2
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
